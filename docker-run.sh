e213cc9eb05a9b0ee7c4f074ac7ed769c0df1d21
Manan M <manan@theboco.in>
2025-10-03 18:00:22 -0400

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,34 +1,42 @@
 #!/bin/sh
 
-if [[ -z "$GID" ]]; then
-	GID="$UID"
+# Railway volumes need root permissions
+if [[ -n "$RAILWAY_ENVIRONMENT" ]]; then
+    UID=0
+    GID=0
 fi
 
-# Define functions.
-function fixperms {
-	chown -R $UID:$GID /data
+if [[ -z "$GID" ]]; then
+    GID="$UID"
+fi
 
-	# /opt/mautrix-linkedin is read-only, so disable file logging if it's pointing there.
-	if [[ "$(yq e '.logging.writers[1].filename' /data/config.yaml)" == "./logs/mautrix-linkedin.log" ]]; then
-		yq -I4 e -i 'del(.logging.writers[1])' /data/config.yaml
-	fi
+# Define fixperms function BEFORE using it
+function fixperms {
+    chown -R $UID:$GID /data
+    
+    # /opt/mautrix-linkedin is read-only, so disable file logging if it's pointing there.
+    if [[ "$(yq e '.logging.writers[1].filename' /data/config.yaml)" == "./logs/mautrix-linkedin.log" ]]; then
+        yq -I4 e -i 'del(.logging.writers[1])' /data/config.yaml
+    fi
 }
 
+# Generate config if missing
 if [[ ! -f /data/config.yaml ]]; then
-	/usr/bin/mautrix-linkedin -c /data/config.yaml -e
-	echo "Didn't find a config file."
-	echo "Copied default config file to /data/config.yaml"
-	echo "Modify that config file to your liking."
-	echo "Start the container again after that to generate the registration file."
-	exit
+    /usr/bin/mautrix-linkedin -c /data/config.yaml -e
+    echo "Didn't find a config file."
+    echo "Copied default config file to /data/config.yaml"
+    echo "Modify that config file to your liking."
+    echo "Start the container again after that to generate the registration file."
+    exit
 fi
 
+# Generate registration if missing
 if [[ ! -f /data/registration.yaml ]]; then
-	/usr/bin/mautrix-linkedin -g -c /data/config.yaml -r /data/registration.yaml || exit $?
-	echo "Didn't find a registration file."
-	echo "Generated one for you."
-	echo "See https://docs.mau.fi/bridges/general/registering-appservices.html on how to use it."
-	exit
+    /usr/bin/mautrix-linkedin -g -c /data/config.yaml -r /data/registration.yaml || exit $?
+    echo "Didn't find a registration file."
+    echo "Generated one for you."
+    echo "See https://docs.mau.fi/bridges/general/registering-appservices.html on how to use it."
+    exit
 fi
 
 cd /data
